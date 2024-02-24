b272125a862fe234361ce7201e81e3c3ae140fec
exec zsh <nicky0115@gmail.com>
2024-02-24 15:31:18 +1100

    remification

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2024_02_24_010736) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,7 +100,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_24_010736) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "grocery_lists", "line_items"
   add_foreign_key "grocery_lists", "users"
   add_foreign_key "item_stores", "items"
   add_foreign_key "item_stores", "stores"
