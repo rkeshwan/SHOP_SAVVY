fb0c150bba01af79bb4cdf8a0d16d5118580965a
Rashika Keshwan <keshwan.r@gmail.com>
2024-02-27 21:05:07 +1100

    quantity updated

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,28 @@
 class LineItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:adjust_item]
+
   def add_item
   end
 
   def adjust_item
+    # @line_item = LineItem.find(params[:id])
+    # @line_item.quantity.update
+
+    # if @line_item.save
+    #   flash[:success] = "Quantity is updated!"
+    # else
+    #   flash[:error] = "Failed to update quantity."
+    # end
+  end
+
+  private
+
+  def line_items_params
+    params.require(:booking).permit(:quantity)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require 'json'
 require "open-uri"
 require "pry-byebug"
 
+puts "Clearing db"
 LineItem.destroy_all
 GroceryList.destroy_all
 Store.destroy_all
@@ -14,31 +15,35 @@ anna = { email: 'anna@gmail.com', password: 'password' }
 jack = { email: 'jack@gmail.com', password: 'password' }
 edith = { email: 'edith@gmail.com', password: 'password' }
 
+puts "Creating users"
 [anna, jack, edith].each do |attributes|
   User.create!(attributes)
 end
 
+# Item database
+puts "Creating items"
+result = File.read(File.join(File.dirname(__FILE__), "all-products.json"))
+JSON.parse(result).each do |item|
+  file = URI.open(item["imagePath"])
+  item = Item.new(name: item["name"], category: item["categoryIds"].first, price: item["price"])
+  item.photo.attach(io: file, filename: "#{item.name}.jpg", content_type: "image/jpg")
+  item.save
+end
+
+# Store database
+puts "Creating stores"
+coles = { name: 'Coles', address: '7 Boundary St, Hawthorn' }
+woolworths = { name: 'Woolworths', address: '56 Salamanca Rd, South Melbourne' }
+organic_oasis = { name: 'Organic Oasis', address: '55 Soho St, Northcote' }
+city_mart = { name: 'City Mart', address: '27 Trafalgar St, Richmond' }
+bliss_foods = { name: 'Bliss Foods', address: '2 Frozen Alley, Cremorne' }
+
+[coles, woolworths, organic_oasis, city_mart, bliss_foods].each do |attributes|
+  Store.create!(attributes)
+end
+
+puts "Creating grocery lists"
 User.all.each do |user|
-  # Item database
-  result = File.read(File.join(File.dirname(__FILE__), "all-products.json"))
-  JSON.parse(result).each do |item|
-    file = URI.open(item["imagePath"])
-    item = Item.new(name: item["name"], category: item["categoryIds"].first, price: item["price"])
-    item.photo.attach(io: file, filename: "#{item.name}.jpg", content_type: "image/jpg")
-    item.save
-  end
-
-  # Store database
-  coles = { name: 'Coles', address: '7 Boundary St, Hawthorn' }
-  woolworths = { name: 'Woolworths', address: '56 Salamanca Rd, South Melbourne' }
-  organic_oasis = { name: 'Organic Oasis', address: '55 Soho St, Northcote' }
-  city_mart = { name: 'City Mart', address: '27 Trafalgar St, Richmond' }
-  bliss_foods = { name: 'Bliss Foods', address: '2 Frozen Alley, Cremorne' }
-
-  [coles, woolworths, organic_oasis, city_mart, bliss_foods].each do |attributes|
-    Store.create!(attributes)
-  end
-
   # Default grocery list - this will exist for the user when they log in to test it
   list = GroceryList.create!(user: user)
   Item.first(5).each do |line_item|
