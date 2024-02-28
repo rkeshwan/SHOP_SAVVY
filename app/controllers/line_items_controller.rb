d538535228775fca9cb46ee7009bc0f0d1631067
Rashika Keshwan <keshwan.r@gmail.com>
2024-02-28 21:34:26 +1100

    Qty adj saved to grocery list

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,28 +1,17 @@
 class LineItemsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:adjust_item]
 
   def add_item
   end
 
   def adjust_item
-    # @line_item = LineItem.find(params[:id])
-    # @line_item.quantity.update
-
-    # if @line_item.save
-    #   flash[:success] = "Quantity is updated!"
-    # else
-    #   flash[:error] = "Failed to update quantity."
-    # end
+    @line_item = LineItem.find(params[:id])
+    @line_item.update(line_item_params)
   end
 
   private
 
-  def line_items_params
-    params.require(:booking).permit(:quantity)
-  end
-
-  def set_user
-    @user = User.find(params[:user_id])
+  def line_item_params
+    params.require(:line_item).permit(:quantity)
   end
 end
