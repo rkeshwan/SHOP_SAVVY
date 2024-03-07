5c2789bef608c0b5ef08327c818bba5dc234ab85
Rashika Keshwan <keshwan.r@gmail.com>
2024-03-07 21:31:18 +1100

    Savings calculated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,7 +18,36 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    #iterate here
+    # raise
+    @grocery_list = GroceryList.find_by(user: current_user)
+    @line_items = @grocery_list.line_items.name
+    # @items = Item.search_by_name_and_category(name: @line_items)
+    # Item_id hash
+    item_id_hash = {}
+    counter = 0
+    @grocery_list.items.each do |item|
+      id = Item.find_by_id(item).id
+      # check each item name in our db, find all the db entries attached to it & find the most expensive price attached to that item.
+      items = Item.where(name: item.name)
+      # highest = items.select { |item|  }
+      highest = items.max_by(&:price)
+      # Take out item price & subtract that from most expensive item found.
+      difference = (highest.price - item.price)
+      # Put this item id & price diff. into our item_hash as a key/value pair
+      item_id_hash[id] = { price: difference }
+    end
+    @grocery_list.line_items.each do |item|
+    # check the hash already created for item_id & once found it'll take the value of the key of the ID & will add to the counter x amount from qty from line_item
+      if item_id_hash[item.item_id]
+        item_difference = item_id_hash[item.item_id][:price] * item.quantity
+        counter += item_difference
+      end
+    # as a result counter variable should total full savings
+    end
+    # @counter goes on the view page
   end
+
 
   def cart
     @grocery_list = GroceryList.find_by(user: current_user)
