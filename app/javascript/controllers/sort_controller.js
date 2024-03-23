f03dd468e2fd3e34653709ae761d33afa9b8c8e4
exec zsh <nicky0115@gmail.com>
2024-03-23 11:00:23 +1100

    sorting

---
+++ app/javascript/controllers/sort_controller.js
@@ -0,0 +1,12 @@
+// import { Controller } from "@hotwired/stimulus"
+
+// // Connects to data-controller="quantity-update"
+// export default class extends Controller {
+//   static targets= ["listSorting"]
+//   sort_asc(event) {
+//     Item.all.sort_by(&:price)
+//   }
+//   sort_desc(event){
+//     Item.all.sort_by{ |item| -item.price }
+//   }
+// }
